258d2042db590834e3ff0c7ac9a8ddfcdc576d54
Harsh Prateek <Itsharshprateek@gmail.com>
2025-12-19 11:29:06 +0530

    Updates in karnataka and homepage created 2 php files adding 3 imp images

---
+++ footer.php
@@ -0,0 +1,78 @@
+<div class="container py-12 xl:!py-16 lg:!py-16 md:!py-16">
+          <div class="flex flex-wrap mx-[-15px] !mt-[-30px] xl:!mt-0 lg:!mt-0">
+            <div class="md:w-4/12 xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
+              <div class="widget">
+                <h4 class="widget-title !mb-3">Name Changed Ads</h4>
+                <p class="!mb-4">© 2025 applynamechange.in. <br class="hidden xl:block lg:block">All rights reserved.</p>
+                <nav class="nav social">
+                  <a class="!text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]" href="#"><i class="uil uil-twitter before:content-['\ed59'] !text-[#5daed5] text-[1rem]"></i></a>
+                  <a class="!text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]" href="#"><i class="uil uil-facebook-f before:content-['\eae2'] !text-[#4470cf] text-[1rem]"></i></a>
+                  <a class="!text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]" href="#"><i class="uil uil-dribbble before:content-['\eaa2'] !text-[#e94d88] text-[1rem]"></i></a>
+                  <a class="!text-[1m] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]" href="#"><i class="uil uil-instagram before:content-['\eb9c'] !text-[#d53581] text-[1rem]"></i></a>
+                  <a class="!text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]" href="#"><i class="uil uil-youtube before:content-['\edb5'] !text-[#c8312b] text-[1rem]"></i></a>
+                </nav>
+                <!-- /.social -->
+              </div>
+              <!-- /.widget -->
+            </div>
+            <!-- /column -->
+            <div class="md:w-4/12 xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
+              <div class="widget">
+                <h4 class="widget-title !mb-3">Our Offices</h4>
+                <ul class="list-unstyled !pl-0">
+                  <li class="!mt-[.35rem]"><a class="!text-[#60697b] hover:!text-[#605dba]" href="#">Mumbai Office: +91 9326098181</a></li>
+                  <li class="!mt-[.35rem]"><a class="!text-[#60697b] hover:!text-[#605dba]" href="#">Bangalore Office: +91 9844879323</a></li>
+                  <li class="!mt-[.35rem]"><a class="!text-[#60697b] hover:!text-[#605dba]" href="#">Chennai Office: +91 9892880035</a></li>
+                </ul>
+              </div>
+              <!-- /.widget -->
+            </div>
+            <!-- /column -->
+            <div class="md:w-4/12 xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
+              <div class="widget">
+                <h4 class="widget-title !mb-3">Name Change Reasons</h4>
+                <ul class="list-unstyled !pl-0">
+                  <li class="!mt-[.35rem]"><a class="!text-[#60697b] hover:!text-[#605dba]" href="#">Spelling Mistake</a></li>
+                  <li class="!mt-[.35rem]"><a class="!text-[#60697b] hover:!text-[#605dba]" href="#">Dislike of Current Name</a></li>
+                  <li class="!mt-[.35rem]"><a class="!text-[#60697b] hover:!text-[#605dba]" href="#">Name Change after Divorce</a></li>
+                  <li class="!mt-[.35rem]"><a class="!text-[#60697b] hover:!text-[#605dba]" href="#">Child's Surname Change</a></li>
+                </ul>
+              </div>
+              <!-- /.widget -->
+            </div>
+            <!-- /column -->
+            <div class="md:w-full xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
+              <div class="widget">
+                <h4 class="widget-title !mb-3">Our Newsletter</h4>
+                <p class="!mb-5">Subscribe to our newsletter to get our news & deals delivered to you.</p>
+                <div class="newsletter-wrapper">
+                  <!-- Begin Mailchimp Signup Form -->
+                  <div id="mc_embed_signup2">
+                    <form action="https://elemisfreebies.us20.list-manage.com/subscribe/post?u=aa4947f70a475ce162057838d&amp;id=b49ef47a9a" method="post" id="mc-embedded-subscribe-form2" name="mc-embedded-subscribe-form" class="validate dark-fields" target="_blank" novalidate>
+                      <div id="mc_embed_signup_scroll2">
+                        <div class="!text-left input-group form-floating !relative flex flex-wrap items-stretch w-full">
+                          <input type="email" value="" name="EMAIL" class="required form-control  relative block w-full text-[.75rem] font-medium !text-[#60697b] bg-[#fefefe] bg-clip-padding border shadow-[0_0_1.25rem_rgba(30,34,40,0.04)] rounded-[0.4rem] border-solid border-[rgba(8,60,130,0.07)] transition-[border-color] duration-[0.15s] ease-in-out     focus:shadow-[0_0_1.25rem_rgba(30,34,40,0.04),unset] focus:!border-[#9fbcf0] focus-visible:!border-[#9fbcf0]   placeholder:!text-[#959ca9] placeholder:opacity-100 m-0 !pr-9 p-[.6rem_1rem] h-[calc(2.5rem_+_2px)] min-h-[calc(2.5rem_+_2px)] !leading-[1.25]" placeholder="" id="mce-EMAIL2">
+                          <label class="!ml-[0.05rem] !text-[#959ca9] text-[.75rem] absolute z-[2] h-full overflow-hidden text-start text-ellipsis whitespace-nowrap pointer-events-none origin-[0_0] px-4 py-[0.6rem] left-0 top-0" for="mce-EMAIL2">Email Address</label>
+                          <input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe2" class="btn btn-grape !text-white !bg-[#605dba] border-[#605dba] hover:text-white hover:bg-[#605dba] hover:!border-[#605dba]   active:text-white active:bg-[#605dba] active:border-[#605dba] disabled:text-white disabled:bg-[#605dba] disabled:border-[#605dba] !relative z-[2] focus:z-[5] hover:!transform-none hover:!translate-none border-0">
+                        </div>
+                        <div id="mce-responses2" class="clear">
+                          <div class="response" id="mce-error-response2" style="display:none"></div>
+                          <div class="response" id="mce-success-response2" style="display:none"></div>
+                        </div> <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ddc180777a163e0f9f66ee014_4b1bcfa0bc" tabindex="-1" value=""></div>
+                        <div class="clear"></div>
+                      </div>
+                    </form>
+                  </div>
+                  <!--End mc_embed_signup-->
+                </div>
+                <!-- /.newsletter-wrapper -->
+              </div>
+              <!-- /.widget -->
+            </div>
+            <!-- /column -->
+          </div>
+          <!--/.row -->
+        </div>
+    <!-- /.container -->
+  </footer>
---
+++ header.php
@@ -0,0 +1,253 @@
+<body class="karnataka-page">
+    <div class="grow shrink-0">
+    <header>
+      <nav class="navbar navbar-expand-lg center-nav transparent position-absolute navbar-light caret-none">
+        <div class="container xl:!flex-row lg:!flex-row !flex-nowrap items-center">
+          <div class="navbar-brand w-full">
+            <a href="./index.html">
+              <img src="./assets/img/namechangeadssmalllogo.png" style="max-height: 40px;" alt="Name Changed Ads">
+            </a>
+          </div>
+          <div class="navbar-collapse offcanvas offcanvas-nav offcanvas-start">
+            <div class="offcanvas-header xl:!hidden lg:!hidden flex items-center justify-between flex-row p-6">
+              <h3 class="!text-white xl:!text-[1.5rem] !text-[calc(1.275rem_+_0.3vw)] !mb-0">Name Changed Ads</h3>
+              <button type="button"
+                class="btn-close btn-close-white !mr-[-0.75rem] m-0 p-0 leading-none !text-[#343f52] transition-all duration-[0.2s] ease-in-out border-0 motion-reduce:transition-none before:text-[1.05rem] before:text-white before:content-['\ed3b'] before:w-[1.8rem] before:h-[1.8rem] before:leading-[1.8rem] before:shadow-none before:transition-[background] before:duration-[0.2s] before:ease-in-out before:!flex before:justify-center before:items-center before:m-0 before:p-0 before:rounded-[100%] hover:no-underline bg-inherit before:bg-[rgba(255,255,255,.08)] before:font-Unicons hover:before:bg-[rgba(0,0,0,.11)]  "
+                data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body xl:!ml-auto lg:!ml-auto flex  flex-col !h-full">
+              <!-- Updated Navigation -->
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link font-bold !tracking-[normal] hover:!text-[#605dba] !text-[.85rem]"
+                    href="./index.html">Home</a>
+                </li>
+                <!-- Services Dropdown -->
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle font-bold !tracking-[normal] hover:!text-[#605dba] !text-[.85rem] active"
+                    href="./services.html" data-bs-toggle="dropdown">Services</a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="./services.html">Affidavit Drafting</a></li>
+                    <li><a class="dropdown-item" href="./services.html">Newspaper Advertisement</a></li>
+                    <li><a class="dropdown-item" href="./services.html">Gazette Notification</a></li>
+                    <li>
+                      <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="./services.html">All Name Change Reasons</a></li>
+                  </ul>
+                </li>
+                <!-- End Services Dropdown -->
+                <li class="nav-item">
+                  <a class="nav-link font-bold !tracking-[normal] hover:!text-[#605dba] !text-[.85rem]"
+                    href="./index.html#process">How It Works</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link font-bold !tracking-[normal] hover:!text-[#605dba] !text-[.85rem]"
+                    href="./about.html">About Us</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link font-bold !tracking-[normal] hover:!text-[#605dba] !text-[.85rem]"
+                    href="./contact.html">Contact Us</a>
+                </li>
+              </ul>
+              <!-- /.navbar-nav -->
+              <div class="offcanvas-footer xl:!hidden lg:!hidden">
+                <div>
+                  <a href="mailto:info@riyoadvertising.com" class="link-inverse">info@riyoadvertising.com</a>
+                  <br> +91 9326098181 <br>
+                  <nav class="nav social social-white !mt-4">
+                    <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]"
+                      href="#"><i class="uil uil-twitter before:content-['\ed59'] !text-white text-[1rem]"></i></a>
+                    <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]"
+                      href="#"><i class="uil uil-facebook-f before:content-['\eae2'] !text-white text-[1rem]"></i></a>
+                    <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]"
+                      href="#"><i class="uil uil-dribbble before:content-['\eaa2'] !text-white text-[1rem]"></i></a>
+                    <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]"
+                      href="#"><i class="uil uil-instagram before:content-['\eb9c'] !text-white text-[1rem]"></i></a>
+                    <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]"
+                      href="#"><i class="uil uil-youtube before:content-['\edb5'] !text-white text-[1rem]"></i></a>
+                  </nav>
+                  <!-- /.social -->
+                </div>
+              </div>
+              <!-- /.offcanvas-footer -->
+            </div>
+            <!-- /.offcanvas-body -->
+          </div>
+          <!-- /.navbar-collapse -->
+          <div class="navbar-other w-full !flex !ml-auto">
+            <ul class="navbar-nav !flex-row !items-center !ml-auto">
+              <li class="nav-item"><a class="nav-link" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-info"><i
+                    class="uil uil-info-circle before:content-['\eb99'] !text-[1.1rem] hover:!text-[#605dba]"></i></a>
+              </li>
+              <li class="nav-item xl:!hidden lg:!hidden">
+                <button class="hamburger offcanvas-nav-btn"><span></span></button>
+              </li>
+            </ul>
+            <!-- /.navbar-nav -->
+          </div>
+          <!-- /.navbar-other -->
+        </div>
+        <!-- /.container -->
+      </nav>
+      <!-- /.navbar -->
+      <div class="offcanvas offcanvas-end text-inverse !text-[#cacaca] opacity-100" id="offcanvas-info"
+        data-bs-scroll="true">
+        <div class="offcanvas-header flex flex-row items-center justify-between p-[1.5rem]">
+          <h3 class="!text-white xl:!text-[1.5rem] !text-[calc(1.275rem_+_0.3vw)] !leading-[1.4] !mb-0">Name Changed Ads
+          </h3>
+          <button type="button" class="btn-close btn-close-white !mr-[-0.5rem] m-0 p-0" data-bs-dismiss="offcanvas"
+            aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body !pb-[1.5rem]">
+          <!-- Fetched Content -->
+          <div class="widget !mb-8">
+            <p>Expert guidance for the complete name change process. We provide a one-stop solution, from affidavit
+              drafting to newspaper ads and Gazette publication, ensuring you meet all legal requirements with ease.</p>
+          </div>
+          <!-- /.widget -->
+          <div class="widget !mb-8">
+            <h4 class="widget-title !text-white !mb-[0.75rem] !text-[.95rem] !leading-[1.45]">Contact Info</h4>
+            <address class=" not-italic !leading-[inherit] !mb-[1rem]"> <strong>Mumbai Office:</strong> 5 Riyo Media
+              House, Patanwala Estate, L.B.S. Marg, Ghatkopar West, Mumbai-400 086</address>
+            <a class="!text-[#cacaca] hover:!text-[#605dba]"
+              href="mailto:info@riyoadvertising.com">info@riyoadvertising.com</a><br> +91 9326098181
+          </div>
+          <!-- /.widget -->
+          <div class="widget !mb-8">
+            <h4 class="widget-title !text-white !mb-[0.75rem] !text-[.95rem] !leading-[1.45]">Learn More</h4>
+            <ul class="list-unstyled !pl-0">
+              <li><a class="!text-[#cacaca] hover:!text-[#605dba]" href="./services.html">All Services</a></li>
+              <li class="!mt-[.35rem]"><a class="!text-[#cacaca] hover:!text-[#605dba]" href="./index.html#process">How It
+                  Works</a>
+              </li>
+              <li class="!mt-[.35rem]"><a class="!text-[#cacaca] hover:!text-[#605dba]" href="./index.html#faq">FAQs</a>
+              </li>
+              <li class="!mt-[.35rem]"><a class="!text-[#cacaca] hover:!text-[#605dba]" href="./about.html">About Us</a>
+              </li>
+            </ul>
+          </div>
+          <!-- /.widget -->
+          <div class="widget">
+            <h4 class="widget-title !text-white !mb-[0.75rem] !text-[.95rem] !leading-[1.45]">Follow Us</h4>
+            <nav class="nav social social-white">
+              <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]"
+                href="#"><i class="uil uil-twitter before:content-['\ed59'] !text-white text-[1rem]"></i></a>
+              <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]"
+                href="#"><i class="uil uil-facebook-f before:content-['\eae2'] !text-white text-[1rem]"></i></a>
+              <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]"
+                href="#"><i class="uil uil-dribbble before:content-['\eaa2'] !text-white text-[1rem]"></i></a>
+              <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]"
+                href="#"><i class="uil uil-instagram before:content-['\eb9c'] !text-white text-[1rem]"></i></a>
+              <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]"
+                href="#"><i class="uil uil-youtube before:content-['\edb5'] !text-white text-[1rem]"></i></a>
+            </nav>
+            <!-- /.social -->
+          </div>
+          <!-- /.widget -->
+          <!-- End Fetched Content -->
+        </div>
+        <!-- /.offcanvas-body -->
+      </div>
+      <!-- /.offcanvas -->
+    </header>
+    <!-- /header -->
+
+    <section class="hero-modern">
+      <div class="hero-noise-overlay"></div>
+      <div class="hero-light-sweep"></div>
+    
+      <div class="hero-content-wrapper">
+        <div class="hero-text-container">
+          <h1 class="hero-title">
+            Gazette Office Name Change In Karnataka
+          </h1>
+          <p class="hero-subtitle">
+            Complete your name change process online. Simple, fast, and fully compliant with all legal requirements for official document updates.
+          </p>
+          <div class="hero-buttons">
+            <a href="https://changeofname.in/download-gazette-name-change-online-application-in-maharashtra/" 
+               class="hero-btn primary">Download Form</a>
+            <a href="tel:+919326098181" 
+               class="hero-btn secondary">Call Now</a>
+          </div>
+        </div>
+        <div class="hero-form-container">
+            <div class="!bg-[#f8f9fa] p-6 md:p-10 rounded-lg">
+              <h2 class="form-section-title !text-[calc(1.3rem_+_0.6vw)] xl:!text-3xl !mb-8">Submit Your Enquiry</h2>
+              <form class="enquiry-form !p-0 !shadow-none !bg-transparent !max-w-full !rounded-none">
+                <div class="form-group">
+                  <label for="oldName"></label>
+                  <input type="text" id="oldName" name="oldName" placeholder="Your old name" required class="!bg-white">
+                </div>
+                <div class="form-group">
+                  <label for="newName"></label>
+                  <input type="text" id="newName" name="newName" placeholder="Your new name" required class="!bg-white">
+                </div>
+                <div class="form-group">
+                  <label for="phoneNumber"></label>
+                  <input type="tel" id="phoneNumber" name="phoneNumber" placeholder="10-digit mobile number"
+                    pattern="[0-9]{10}" maxlength="10" required class="!bg-white">
+                  <small></small>
+                </div>
+                <div class="form-group">
+                  <label for="reason"></label>
+                  <select id="reason" name="reason" required class="!bg-white">
+                    <option value="">-- Select --</option>
+                    <option value="marriage">Marriage</option>
+                    <option value="astrology">Astrology</option>
+                    <option value="passport">Passport Issue</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="location"></label>
+                  <input type="text" id="location-form" name="location" placeholder="City / State" required
+                    class="!bg-white">
+                </div>
+                <button type="submit" class="submit-button">Submit Enquiry</button>
+              </form>
+            </div>
+        </div>
+      </div>
+    </section>
+    
+    <section class="trust-bar">
+  <div class="trust-inner">
+
+    <!-- Google Rating -->
+    <div class="trust-google">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" alt="Google Reviews"  class="google-logo"/>
+      <div class="stars">★★★★★</div>
+      <span>4.7 • 581 Reviews</span>
+    </div>
+
+    <!-- Stats -->
+    <div class="trust-stats">
+      <div class="trust-item">
+        <span class="icon">🏆</span>
+        <strong>5000+</strong>
+        <small>Successful Changes</small>
+      </div>
+
+      <div class="trust-item">
+        <span class="icon">🛡️</span>
+        <strong>100%</strong>
+        <small>Work Guarantee</small>
+      </div>
+
+      <div class="trust-item">
+        <span class="icon">💰</span>
+        <strong>100%</strong>
+        <small>Lowest Cost</small>
+      </div>
+
+      <div class="trust-item">
+        <span class="icon">⭐</span>
+        <strong>100%</strong>
+        <small>Best Service</small>
+      </div>
+    </div>
+
+  </div>
+</section>
